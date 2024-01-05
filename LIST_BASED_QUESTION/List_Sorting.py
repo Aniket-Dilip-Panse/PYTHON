5c93ce81dca26f3d6acf71b2e83abca66d39008e
ANIKET_PANSE <84241376+Aniket-Dilip-Panse@users.noreply.github.com>
2024-01-05 19:08:51 +0530

    list sorting in increasing order by the last element

---
+++ LIST_BASED_QUESTION/List_Sorting.py
@@ -0,0 +1,8 @@
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+def sorting_list(my_list):
+    my_list.sort(key=lambda x: x[-1])
+    return my_list
+print(sorting_list([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
